cd8efe3d2797c5778cdbf261ac2c99ea41c73f9c
Juan Pablo Guzman <jguzman@nearsoft.com>
2019-11-10 14:32:13 -0600

    Added recharts library

--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { LineChart, Line, XAxis, YAxis } from 'recharts';
 
 const useStyles = makeStyles({
   card: {
@@ -9,10 +10,15 @@ const useStyles = makeStyles({
 
 export default () => {
   const classes = useStyles();
+  const data = [{name: 'Page A', at: 400, pv: 2400, amt: 2400}, {name: 'Page A', at: 500, pv: 2800, amt: 2900}];
 
   return (
     <div className={classes.card}>
-      Chart goes here
+      <LineChart width={800} height={800} data={data}>
+        <YAxis label={{ angle: -90, value: 'values' }} />
+        <XAxis dataKey="at" interval="preserveStartEnd" minTickGap={20} />
+        <Line type="monotone" dataKey="at" stroke="#8884d8" />
+      </LineChart>
     </div>
   );
 };
