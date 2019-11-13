fe311ef315ab3ae697864b3a9f468bdf68f2b3a6
Juan Pablo Guzman <jguzman@nearsoft.com>
2019-11-13 13:05:59 -0600

    Changed the colors pallete

--- src/store/reducers/ColorsReducer.tsx
+++ src/store/reducers/ColorsReducer.tsx
@@ -1,6 +1,6 @@
 
 
-const initialState = ['red', 'blue', 'brown', 'orange', 'gray', 'green', 'purple'];
+const initialState = ['#3498db', '#27ae60', '#e67e22', '#7f8c8d', '#8e44ad','#2c3e50', 'gray'];
 
 export const colorsReducer = (state: any = initialState, action: any) => {
   //TODO: add functionality to be able to change colors
