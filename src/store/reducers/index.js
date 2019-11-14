ae9f7742ea050142dfbad2ac1bfdf4a60b940326
Juan Pablo Guzman <jguzman@nearsoft.com>
2019-11-14 15:03:19 -0600

    Deleted weather Feature

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,11 +1,9 @@
-import { reducer as weatherReducer } from '../../Features/Weather/reducer';
 import { MetricsReducer } from './MetricsReducer';
 import { ChartReducer } from './ChartReducer';
 import { ColorsReducer } from './ColorsReducer';
 import { LatestMetricsValuesReducer } from './LatestMetricValuesReducer';
 
 export default {
-  weather: weatherReducer,
   metrics: MetricsReducer,
   chartData: ChartReducer,
   colors: ColorsReducer,
